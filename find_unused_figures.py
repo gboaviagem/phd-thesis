122bfe6c711b1aeb352b8328af7f64c2c97071fa
gboaviagem@air <guilherme.boaviagem@gmail.com>
2022-07-17 10:08:55 -0300

    Update README and python script.

--- find_unused_figures.py
+++ find_unused_figures.py
@@ -12,12 +12,12 @@ figures = [
 tex_files = [f for f in os.listdir(thesis_folder) if f.endswith(".tex")]
 
 for fig in figures:
-    found = False
+    FOUND = False
     for tex_file in tex_files:
         with open(f"{thesis_folder}/{tex_file}", "r") as f:
             content = f.read()
         if fig in content:
-            found = True
-    if not found:
+            FOUND = True
+    if not FOUND:
         os.remove(f"{fig_folder}/{fig}")
         print(f"{fig} was removed, as it was not used in TeX files.")
